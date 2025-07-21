d43d5bcc5039e7802d4766d89dd9366dedf4c5d4
Ariel <arina.kudar@gmail.com>
2025-07-21 17:54:37 +0300

    fix

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,15 +6,42 @@ import {
   updateContact,
   deleteContactById
 } from '../services/contacts.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 export const getContactsController = async (req, res) => {
-	  const contacts = await getAllContacts();
+  const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+  const filter = parseFilterParams(req.query);
 
-	  res.status(200).json({
-	    status: 200,
-	    message: 'Successfully found contacts!',
-	    data: contacts,
-	  });
+	const {
+    data,
+    totalItems,
+    totalPages,
+    hasPreviousPage,
+    hasNextPage,
+  } = await getAllContacts({
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+    filter,
+  });
+
+	res.status(200).json({
+	  status: 200,
+	  message: 'Successfully found contacts!',
+    data: {
+        data,
+        page: parseInt(page),
+        perPage: parseInt(perPage),
+        totalItems,
+        totalPages,
+        hasPreviousPage,
+        hasNextPage,
+    }
+	});
 };
 
 export const getContactByIdController = async (req, res, next) => {
---
+++ src/validation/contacts.js
@@ -0,0 +1,15 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email().min(3).max(20).required(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().min(3).max(20).required(),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20),
+  email: Joi.string().email(),
+  phone: Joi.string().min(3).max(20),
+}).or('name', 'email', 'phone');
