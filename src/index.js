8169722a7b9a0607f6533586d0d825082d902be9
Ariel <arina.kudar@gmail.com>
2025-08-12 16:54:17 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import { initMongoConnection } from './db/initMongoConnection.js';
-import { startServer } from './server.js';
+import { setupServer } from './server.js';
 import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 
@@ -10,7 +10,7 @@ const bootstrap = async () => {
   await initMongoConnection();
   await createDirIfNotExists(TEMP_UPLOAD_DIR);
   await createDirIfNotExists(UPLOAD_DIR);
-  startServer();
+  setupServer();
 };
 
 void bootstrap();
